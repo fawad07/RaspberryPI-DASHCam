30636afc0c0fd14bc84d42e4983e24cba81b83c8
Muhammad Fawad Aleem <muhammad.f.aleem@gmail.com>
2020-07-01 19:36:03 -0700

    Update cam.py

--- cam.py
+++ cam.py
@@ -9,6 +9,31 @@ dialog=tk.Tk()       #main Window
 cam=PiCamera()
 time=str(datetime.now().strftime("%c %A"))
 
+def main():
+    dialog.geometry("300x275+5+5")
+    btn_frame = tk.Frame(dialog)
+    btn_frame.pack(fill=tk.X, side=tk.BOTTOM)
+
+    pic_btn = tk.Button(btn_frame, text="take pic", command=camera_take_pic)
+    pic_btn.grid(row=0, column=0, sticky=tk.W + tk.E)
+    # pic_btn.pack()
+
+    vid_rec_btn = tk.Button(btn_frame, text="record", command=camera_record)
+    vid_rec_btn.grid(row=0, column=1, sticky=tk.W + tk.E)
+    # vid_btn.pack()
+    stop_vid_rec = tk.Button(btn_frame, text="Stop recording", command=stop_record)
+    stop_vid_rec.grid(row=1, column=1, sticky=tk.W + tk.E)
+
+    close_btn = tk.Button(btn_frame, text="Close", command=close_window)
+    close_btn.grid(row=0, column=2, sticky=tk.W + tk.E)
+
+    btn_frame.columnconfigure(0, weight=1)
+    btn_frame.columnconfigure(1, weight=1)
+    btn_frame.columnconfigure(2, weight=1)
+    camera_record()
+    tk.mainloop();
+
+
 def camera_take_pic():
     #cam=PiCamera()
     cam.resolution = (1024, 768)
@@ -25,6 +50,8 @@ def camera_take_pic():
 def camera_record():
     #cam=PiCamera();
     cam.rotation = 90;
+    cam.sharpness = 100;
+    cam.zoom(70,80, 1, 1)
     cam.resolution = (1024, 768);
     cam.start_recording('/home/pi/Desktop/test.h264');
     cam.preview_fullscreen=False
@@ -46,25 +73,30 @@ def close_window():
     cam.close();
     dialog.destroy();
 
-#_______________________________________________________________________
-dialog.geometry("300x275+5+5")
-btn_frame=tk.Frame(dialog)
-btn_frame.pack(fill=tk.X, side=tk.BOTTOM)
 
-pic_btn=tk.Button(btn_frame, text="take pic", command=camera_take_pic)
-pic_btn.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+#_______________________________________________________________________
+if __name__ == "__main__":
+    main()
+
+#dialog.geometry("300x275+5+5")
+#btn_frame=tk.Frame(dialog)
+#btn_frame.pack(fill=tk.X, side=tk.BOTTOM)
+
+#pic_btn=tk.Button(btn_frame, text="take pic", command=camera_take_pic)
+#pic_btn.grid(row=0, column=0, sticky=tk.W+tk.E)
 #pic_btn.pack()
 
-vid_rec_btn=tk.Button(btn_frame, text="record", command=camera_record)
-vid_rec_btn.grid(row=0, column=1, sticky=tk.W+tk.E)
+#vid_rec_btn=tk.Button(btn_frame, text="record", command=camera_record)
+#vid_rec_btn.grid(row=0, column=1, sticky=tk.W+tk.E)
 #vid_btn.pack()
-stop_vid_rec=tk.Button(btn_frame, text="Stop recording", command=stop_record)
-stop_vid_rec.grid(row=1, column=1, sticky=tk.W+tk.E)
+#stop_vid_rec=tk.Button(btn_frame, text="Stop recording", command=stop_record)
+#stop_vid_rec.grid(row=1, column=1, sticky=tk.W+tk.E)
 
-close_btn=tk.Button(btn_frame, text="Close", command=close_window)
-close_btn.grid(row=0, column=2, sticky=tk.W+tk.E)
+#close_btn=tk.Button(btn_frame, text="Close", command=close_window)
+#close_btn.grid(row=0, column=2, sticky=tk.W+tk.E)
 
-btn_frame.columnconfigure(0, weight=1)
-btn_frame.columnconfigure(1, weight=1)
-btn_frame.columnconfigure(2, weight=1)
-tk.mainloop();
+#btn_frame.columnconfigure(0, weight=1)
+#btn_frame.columnconfigure(1, weight=1)
+#btn_frame.columnconfigure(2, weight=1)
+#tk.mainloop();
