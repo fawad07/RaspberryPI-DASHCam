753a03fc9b457bf2eeaa58ca741ee3f611a9582b
Muhammad Fawad Aleem <muhammad.f.aleem@gmail.com>
2020-09-09 14:05:26 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #import tkinter as tk;
 #from time import sleep;
 
-#chmod u+x /home/pi/Documents/cam.py;
-python3 /home/pi/Documents/cam.py
+
+python3 /home/pi/workspace/python/RaspberryPI-DASHCAM/cam.py
 
